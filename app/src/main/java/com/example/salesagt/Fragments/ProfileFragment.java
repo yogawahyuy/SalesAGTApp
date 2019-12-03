8825815006cfff10cf205ffa3d1825568c834d61
Yogawahyuy4058 <37763772+Yogawahyuy4058@users.noreply.github.com>
2019-12-03 17:13:19 +0700

    Notif

--- app/src/main/java/com/example/salesagt/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/salesagt/Fragments/ProfileFragment.java
@@ -11,10 +11,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.salesagt.LoginActivity;
 import com.example.salesagt.Model.UserModel;
 import com.example.salesagt.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -34,6 +41,7 @@ public class ProfileFragment extends Fragment {
     Button btnLogout;
     View v;
     FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleSignInClient;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -58,10 +66,34 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
+                if (mGoogleSignInClient!=null){
+                    Auth.GoogleSignInApi.signOut(mGoogleSignInClient).setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(@NonNull Status status) {
+
+                        }
+                    });
+                }
                 startActivity(new Intent(getContext(),LoginActivity.class));
                 getActivity().finish();
             }
         });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        if (mGoogleSignInClient == null){
+            mGoogleSignInClient = new GoogleApiClient.Builder(getContext())
+                    .enableAutoManage(getActivity(), new GoogleApiClient.OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                            Toast.makeText(getActivity(), "Something Error", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                    .build();
+        }
 
         return v;
     }
--- app/src/main/java/com/example/salesagt/Model/NotifModel.java
+++ app/src/main/java/com/example/salesagt/Model/NotifModel.java
@@ -3,5 +3,46 @@ package com.example.salesagt.Model;
 import java.io.Serializable;
 
 public class NotifModel implements Serializable {
-    
+    String id,title,message,date;
+
+    public NotifModel(String title, String message, String date) {
+        this.title = title;
+        this.message = message;
+        this.date = date;
+    }
+
+    public NotifModel() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
